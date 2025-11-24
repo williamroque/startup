a268a7d72a91170a3a34c5f7d9c11978983c6bca
William Roque <william.aroque@gmail.com>
2025-11-24 19:44:04 -0300

    Added more tips.

--- src/tip/Tip.jsx
+++ src/tip/Tip.jsx
@@ -4,10 +4,12 @@ import './Tip.css';
 
 const tips = {
     'go-studio': 'Go to the Studio tab to create new frames.',
-    'explain-studio': 'Each input square below corresponds to a space on the frame. Select characters to draw their representations.',
-    'explain-dictionary': '',
-    'go-discovery': '',
-    'explain-discovery': ''
+    'explain-studio': 'Each input square below corresponds to a space on the frame (top left, top middle, etc.). Select characters to draw their representations.',
+    'explain-dictionary': 'This is a list of all the characters you have discovered so far. Click on an entry to learn how to write the character and see its picture representation.',
+    'go-discover': 'Go to the Discover tab in order to find new characters to use in your paintings.',
+    'explain-discover': 'Write one of the characters in your dictionary by hand in the canvas below. Then, click the Insert button to save the detected character to one of the white spaces. If the two characters you insert form a valid combo, you will unlock a new character!',
+    'explain-discover-buttons': 'Click Clear Canvas to erase canvas contents. Click Clear All to erase the canvas and the white spaces.',
+    'explain-visit': 'Click on a person\'s username to visit their gallery!'
 };
 
 export default function Tip({ tipID }) {
