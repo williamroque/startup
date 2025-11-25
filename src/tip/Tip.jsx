8e8b2ebfe090efa78937764dcfc27c22ad8a0548
William Roque <william.aroque@gmail.com>
2025-11-24 21:53:04 -0300

    Mentioned haiku in discover hint.

--- src/tip/Tip.jsx
+++ src/tip/Tip.jsx
@@ -7,7 +7,7 @@ const tips = {
     'explain-studio': 'Each input square below corresponds to a space on the frame (top left, top middle, etc.). Select characters to draw their representations.',
     'explain-dictionary': 'This is a list of all the characters you have discovered so far. Click on an entry to learn how to write the character and see its picture representation.',
     'go-discover': 'Go to the Discover tab in order to find new characters to use in your paintings.',
-    'explain-discover': 'Write one of the characters in your dictionary by hand in the canvas below. Then, click the Insert button to save the detected character to one of the white spaces. If the two characters you insert form a valid combo, you will unlock a new character!',
+    'explain-discover': 'Write one of the characters in your dictionary by hand in the canvas below. Then, click the Insert button to save the detected character to one of the white spaces. If the two characters you insert form a valid combo, you will unlock a new character! The haiku is a hint for a new combo.',
     'explain-discover-buttons': 'Click Clear Canvas to erase canvas contents. Click Clear All to erase the canvas and the white spaces.',
     'explain-visit': 'Click on a person\'s username to visit their gallery!'
 };
